39751598700af5b2b31f872963659d310b1232fa
eklnc <emirkilinc@hotmail.com>
2021-09-07 01:26:10 +0300

    v3 added

--- ApplicationDomainExample/ApplicationDomainExample/Program.cs
+++ ApplicationDomainExample/ApplicationDomainExample/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security;
+using System.Security.Permissions;
+
 using static System.Console;
 
 namespace ApplicationDomainExample
@@ -12,11 +10,38 @@ namespace ApplicationDomainExample
 	{
 		static void Main(string[] args)
 		{
-			// other app domain
-			var thirdParty = new ThirdPartyPackage();
-			var exampleAppDomain = AppDomain.CreateDomain("ExampleAppDomain");
-			var thirdPartyType = typeof(ThirdPartyPackage);
-			exampleAppDomain.CreateInstanceAndUnwrap(thirdPartyType.Assembly.FullName, thirdPartyType.FullName);
+			// permissions 
+			var perm = new PermissionSet(PermissionState.None);
+			perm.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution));
+			perm.AddPermission(new FileIOPermission(FileIOPermissionAccess.NoAccess, @"c:\"));
+
+			var setup = new AppDomainSetup
+			{
+				ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase
+			};
+
+			// other app domain which secured c:\
+			var securedAppDomain = AppDomain.CreateDomain("SecuredAppDomain", null, setup, perm);
+			//var thirdPartyType = typeof(ThirdPartyPackage);
+
+			//// you will see constructor log but 
+			//// you got System.Security.SecurityException now
+			//securedAppDomain.CreateInstanceAndUnwrap(thirdPartyType.Assembly.FullName, thirdPartyType.FullName);
+
+			try
+			{
+				var thirdPartyType = typeof(ThirdPartyPackage);
+
+
+				// you got "System.Security.SecurityException" still but you unloaded the corresponding app domain now.
+				securedAppDomain.CreateInstanceAndUnwrap(thirdPartyType.Assembly.FullName, thirdPartyType.FullName);
+			}
+			catch (Exception ex)
+			{
+				// maybe, you want to log the exception
+
+				AppDomain.Unload(securedAppDomain);
+			}
 
 			// current app domain
 			var exampleClass = new ExampleClass();
